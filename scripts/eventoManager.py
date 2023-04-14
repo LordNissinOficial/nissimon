374ae9ba158f3bfa18fb3ce6c94c8babc99a6642
* <lord.nissin.oficial@gmail.com>
2023-04-13 22:48:14 -0300

    adicionado delay nos eventos •agora pode ter delay entre acoes nos eventos

--- scripts/eventoManager.py
+++ scripts/eventoManager.py
@@ -12,16 +12,20 @@ class EventoManager():
 	def update(self):
 		#print("rodando o script")
 		if self.lista:
-#			if self.timerInicial:
-#				if time()-
+			if self.timerInicial:
+				if time()-self.timerInicial>=self.timerDuracao:
+					self.timerInicial = 0
+					self.timerDuracao = 0
+					self.terminouAcao =True
 			if not self.comecouAcao:
 				print("comecou acao", self.lista[0])
-				#if type(self.lista[0])==str:
-#					if self.lista[0].split(" ")[0]=="espere":
-#						self.timerInicial = time()
-#						self.timerDuracao = float(self.lista[0].split(" ")[1])
-				self.lista[0]()
 				self.comecouAcao = True
+				if type(self.lista[0])==str:
+					if self.lista[0].split(" ")[0]=="espere":
+						self.timerInicial = time()
+						self.timerDuracao = float(self.lista[0].split(" ")[1])
+						return
+				self.lista[0]()				
 	
 			if self.terminouAcao:
 				print("terminou acao")
--- scripts/eventos/casaTestesEventos.py
+++ scripts/eventos/casaTestesEventos.py
@@ -1,10 +1,14 @@
 import time
 def eventoTeste(jogo, lista):
+	lista.append("espere 0.5")
 	lista.append(lambda: jogo.moverJogador(-1, 0, emEvento=True))
-	#lista.append("espere 1")
+	lista.append("espere 0.5")
 	lista.append(lambda: jogo.moverJogador(-1, 0, emEvento=True))
+	lista.append("espere 0.5")
 	lista.append(lambda: jogo.moverJogador(0, -1, emEvento=True))
+	lista.append("espere 0.5")
 	lista.append(lambda: jogo.moverJogador(0, -1, emEvento=True))
+	lista.append("espere 0.5")
 	lista.append(lambda: jogo.a(emEvento=True))
 	#lista.append(lambda: jogo.moverJogador(0, 1, emEvento=True))
 	#jogo.moverJogador(-1, 0)
